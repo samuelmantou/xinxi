4dd4d03b1dc26fe0ac440dcf14f573e64169be3e
samuelmantou <samuelmantou@gmail.com>
2022-05-04 09:25:27 +0800

    a

--- pkg/pintuan_test.go
+++ pkg/pintuan_test.go
@@ -2,7 +2,6 @@ package pkg
 
 import (
 	"context"
-	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
@@ -24,57 +23,19 @@ func TestA(t *testing.T) {
 	log.Println(c)
 }
 
-func newTimeTicker(ctx context.Context, start, end string, insert time.Duration) {
-	n := time.Now()
-	nDay := n.Format("2006-01-02")
-	layout := "2006-01-02 15:04"
-	sTime, err := time.ParseInLocation(layout, fmt.Sprintf("%s %s", nDay, start), time.Local)
-	if err != nil {
-		log.Println(err)
-	}
-	eTime, err := time.ParseInLocation(layout, fmt.Sprintf("%s %s", nDay, end), time.Local)
-	if err != nil {
-		log.Println(err)
-	}
-	t1 := sTime.Sub(n)
-	t2 := eTime.Sub(n)
-	var gap time.Duration
-	if t1 > 0 {
-		// 还没开始
-		gap = sTime.Add(insert).Sub(n)
-	}else if t2 < 0 {
-		// 已经结束
-		gap = sTime.Add(time.Hour * 24).Sub(n) + insert
-	}else {
-		// 已经开始
-		next := sTime.Add((t1 / insert - 1) * -insert)
-		gap = next.Sub(n)
-	}
-	var timer *time.Timer
-
-	for {
-		log.Println("时间开始")
-		timer = time.NewTimer(gap)
-		select {
-		case <-timer.C:
-			log.Println("时间结束")
-			gap = insert
-			if time.Now().Add(gap).Sub(eTime) > 0 {
-				gap += sTime.Add(time.Hour * 24).Sub(eTime)
-			}
-			timer.Stop()
-		case <-ctx.Done():
-			log.Println("回收运行")
-			timer.Stop()
-			return
-		}
-	}
-}
-
 func TestTime(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
-	go newTimeTicker(ctx, "8:00", "20:26", time.Minute * 2)
-	time.Sleep(time.Second * 5)
+	nC := make(chan Notice)
+	newTimeTicker(ctx, nC, "8:00", "20:26", time.Minute * 2)
+	go func() {
+		for {
+			select {
+			case <-nC:
+				log.Println("收到信号")
+			}
+		}
+	}()
+	time.Sleep(time.Minute * 5)
 	cancel()
 	<-make(chan struct{})
 }
--- pkg/ticker.go
+++ pkg/ticker.go
@@ -2,8 +2,62 @@ package pkg
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"time"
 )
+
+type Notice struct{}
+
+func newTimeTicker(ctx context.Context, nC chan <-Notice, start, end string, insert time.Duration) {
+	n := time.Now()
+	nDay := n.Format("2006-01-02")
+	layout := "2006-01-02 15:04"
+	sTime, err := time.ParseInLocation(layout, fmt.Sprintf("%s %s", nDay, start), time.Local)
+	if err != nil {
+		log.Println(err)
+	}
+	eTime, err := time.ParseInLocation(layout, fmt.Sprintf("%s %s", nDay, end), time.Local)
+	if err != nil {
+		log.Println(err)
+	}
+	t1 := sTime.Sub(n)
+	t2 := eTime.Sub(n)
+	var gap time.Duration
+	if t1 > 0 {
+		// 还没开始
+		gap = sTime.Add(insert).Sub(n)
+	}else if t2 < 0 {
+		// 已经结束
+		gap = sTime.Add(time.Hour * 24).Sub(n) + insert
+	}else {
+		// 已经开始
+		next := sTime.Add((t1 / insert - 1) * -insert)
+		gap = next.Sub(n)
+	}
+
+	go func() {
+		var timer *time.Timer
+		for {
+			log.Println("时间开始")
+			timer = time.NewTimer(gap)
+			select {
+			case <-timer.C:
+				gap = insert
+				if time.Now().Add(gap).Sub(eTime) > 0 {
+					gap += sTime.Add(time.Hour * 24).Sub(eTime)
+				}
+				timer.Stop()
+				nC<- struct{}{}
+				log.Println("时间结束")
+			case <-ctx.Done():
+				log.Println("回收运行")
+				timer.Stop()
+				return
+			}
+		}
+	}()
+}
 
 func (p *PinTuan) pdTicker(ctx context.Context) {
 	for {
